4241443e68a9fcaafca42023b2e7f67f470e6831
LukeZhuang <>
2024-01-24 22:35:56 -0500

    configure my own color theme for diff mode

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,9 +1,21 @@
 -- define your colorscheme here
-local colorscheme = 'monokai_pro'
+local monokai = require('monokai')
+local palette = monokai.pro
+monokai.setup {
+    palette = {
+        diff_add = '#bada9f',
+        diff_remove = '#ff8080',
+        -- diff_change = '#8b4513',
+        diff_change = '#f4a460',
+        diff_text = '#74baec',
+    }
+}
 
-local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not is_ok then
-    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
-    return
-end
+-- local colorscheme = 'monokai_pro'
+
+-- local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not is_ok then
+--     vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+--     return
+-- end
 
